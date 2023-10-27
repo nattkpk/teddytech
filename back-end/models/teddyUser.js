bd58652e24ebc76d0c42008c81b546176f434824
Imweeraya <weerayakosaldilok@gmail.com>
2023-10-27 10:15:03 +0700

    connect mongoDB

--- back-end/models/teddyUser.js
+++ back-end/models/teddyUser.js
@@ -40,6 +40,5 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', UserSchema);
 
-module.exports = User;
+module.exports =  mongoose.model('TeddyUser', UserSchema);;
