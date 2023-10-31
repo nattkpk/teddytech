2fbb8428f8e2dd9aaabbca8525dd741ae50b70f8
nattkpk <nattkpk@gmail.com>
2023-10-31 20:41:00 +0700

    FIX : profile and ADD : mat-angular

--- font-end/src/app/app.module.ts
+++ font-end/src/app/app.module.ts
@@ -1,13 +1,16 @@
+//module
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ModelModule } from './models/userModel/model.module';
-import { Signin } from './components/signin/signin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ServiceModule } from './services/model.module';
+//component
+import { AppComponent } from './app.component';
 import {Board} from './components/board/board.component'
+import { Signin } from './components/signin/signin.component';
 import { EditActivity } from './components/editActicitySticker/editActivity.component';
-import { FormsModule } from '@angular/forms';
 import { EditReward } from './components/editRewardSticker/editReward.component';
 import { EditFeeling } from './components/editFeelingSticker/editFeeling.component';
 import { EditPraise } from './components/editPraiseSticker/editPraise.component';
@@ -15,10 +18,14 @@ import { EditPoint } from './components/editPointSticker/editPoint.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { ServiceModule } from './services/model.module';
 import { HowtouseComponent } from './components/howtouse/howtouse.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+
+// material
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 @NgModule({
   declarations: [
@@ -44,6 +51,8 @@ import { EditProfileComponent } from './components/edit-profile/edit-profile.com
     BrowserModule,
     DragDropModule,
     AppRoutingModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- font-end/src/app/components/profile/profile.component.ts
+++ font-end/src/app/components/profile/profile.component.ts
@@ -8,6 +8,23 @@ import Swal from 'sweetalert2';
 })
 export class ProfileComponent {
   showEditProfile: boolean = false;
+  determinate1: number = 0;
+  targetValue1: number = 80;
+  determinate2: number = 0;
+  targetValue2: number = 80;
+
+
+  constructor() { }
+
+  async ngOnInit(): Promise<void> {
+    await setInterval(() => {
+      this.determinate1 = this.targetValue1;
+    }, 500);
+    await setInterval(() => {
+      this.determinate2 = this.targetValue2;
+    }, 600);
+  }
+
 
   editProfile() {
     Swal.fire({
@@ -21,5 +38,13 @@ export class ProfileComponent {
       confirmButtonColor: '#A1C554',
       cancelButtonColor: '#FC6F6F',
     });
+
+   
+
+    
+
+
+
+
   }
 }
